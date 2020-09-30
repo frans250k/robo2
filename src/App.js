142b685187726f20a944d7956b14084ebe29e056
Frans Kbarek <63740351+frans250k@users.noreply.github.com>
2020-10-01 01:41:11 +0900

    tambah component

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Tes code-sandbox!!</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Tes code!!</h1>
     </div>
   );
 }
