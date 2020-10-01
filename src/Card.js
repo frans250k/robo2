bec7069f7cf2cd46bad1610874ca797b94fa9b1a
Frans Kbarek <63740351+frans250k@users.noreply.github.com>
2020-10-01 09:02:25 +0900

    komponen oke, baru jadi statis

--- src/Card.js
+++ src/Card.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Card = () => {
   return (
-    <div>
-      <img src="https://robohash.org/random?200x200" alt="robots" />
+    <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img src="https://robohash.org/kikuk?200x200" alt="robots" />
       <div>
         <h2>Jane doe</h2>
         <p>janedoe@gmail.com</p>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "tachyons";
 
 import Card from "./Card";
+import "tachyons";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
+    <Card />
+    <Card />
+    <Card />
     <Card />
   </React.StrictMode>,
   rootElement
