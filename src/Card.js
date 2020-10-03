f4ab23c49d1eddd60966f8324a2566675829161b
Frans Kbarek <63740351+frans250k@users.noreply.github.com>
2020-10-03 12:50:00 +0900

    ubah judul font

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-const Card = () => {
+const Card = (props) => {
+  const { name, email, id } = props;
   return (
     <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img src="https://robohash.org/kikuk?200x200" alt="robots" />
+      <img src={`https://robohash.org/${id}?200x200`} alt="robots" />
       <div>
-        <h2>Jane doe</h2>
-        <p>janedoe@gmail.com</p>
+        <h2>{name}</h2>
+        <p>{email}</p>
       </div>
     </div>
   );
